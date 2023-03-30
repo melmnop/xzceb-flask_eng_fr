c7b567d20ae00e0d16c05eb7918d5c7b7e281ea3
melmnop <129010500+melmnop@users.noreply.github.com>
2023-03-29 20:55:08 -0400

    Update test_translator.py

--- final_project/test_translator.py
+++ final_project/test_translator.py
@@ -6,14 +6,14 @@ class TestTranslator(unittest.TestCase):
   
   def test_english_to_french(self):
     self.assertEqual(english_to_french('hello'), 'bonjour')
-    self.assertEqual(english_to_french('goodbye'), 'au revoir')
     self.assertEqual(english_to_french('good'), 'bien')
     self.assertNotEqual(english_to_french(0), 0)
+    self.assertNotEqual(english_to_french('None'), '')
     
   def test_french_to_english(self):
     self.assertEqual(french_to_english('bien'), 'good')
-    self.assertEqual(french_to_english('au revoir'), 'goodbye')
     self.assertNotEqual(french_to_english(0), 0)
+    self.assertNotEqual(french_to_english('None'), '')                    
     self.assertEqual(french_to_english('bonjour'), 'hello')
 
 unittest.main()
