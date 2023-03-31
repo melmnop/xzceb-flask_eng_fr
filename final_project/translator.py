86bb84c9a62fcc693ce239652c74ceb208b57ba4
melmnop <129010500+melmnop@users.noreply.github.com>
2023-03-31 18:19:43 -0400

    Update translator.py

--- final_project/translator.py
+++ final_project/translator.py
@@ -1,6 +1,6 @@
 """Two functions that will use Watson Translator to translate"""
-import json
-import os
+#import json
+#import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
@@ -11,22 +11,20 @@ load_dotenv()
 authenticator = IAMAuthenticator('lW5nKHZ-dascQb4ZpRVt-2XKz9HrCOK2wTiOaXTCxYeF')
 
 language_translator = LanguageTranslatorV3(
-    version = '2018-05-01', 
-    authenticator=authenticator
-)
-
+    version = '2018-05-01', authenticator=authenticator)
 language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com/\
     instances/4ecc31ff-3cae-45d7-bb3b-13b99049deac')
 
-
-def english_to_french(englishText):
+def english_to_french(english_text):
     """Function translates English to French"""
-    translation = language_translator.translate(text = englishText, model_id = 'en-fr').get_result()
+    translation = language_translator.translate(
+    text = english_text, model_id = 'en-fr').get_result()
     french_text = translation['translations'][0]['translation']
     return french_text
 
-def french_to_english(frenchText):
+def french_to_english(french_text):
     """Function translates French to English"""
-    translation = language_translator.translate(text = frenchText, model_id = 'fr-en').get_result()
+    translation = language_translator.translate(
+    text = french_text, model_id = 'fr-en').get_result()
     english_text = translation['translations'][0]['translation']
-    return english_text                                                                                                    
+    return english_text 
